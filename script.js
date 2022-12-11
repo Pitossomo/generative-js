c5d182972b8b3f6e7db8185a5a8d8de809467627
pitossomo <pedro.h.teles@hotmail.com>
2022-12-11 00:06:19 -0300

    spirals done in script.js

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   background('black')
   stroke('white')
+  strokeWeight(2)
 
-  const NUM_POINTS = 100
+  const NUM_POINTS = 200
   const NUM_ROUNDS = 5
 
   let point1 = [windowWidth/2, windowHeight/2]
   for (let i = 0; i < NUM_POINTS; i++) {
     point2 = toCartesianCoordinates(
-      Math.min(windowWidth,windowHeight)/2/100*i + windowWidth/2,
-      2*Math.PI*NUM_ROUNDS/NUM_POINTS + windowHeight/2
+      i/NUM_POINTS*Math.min(windowWidth,windowHeight)/2,
+      2*Math.PI*NUM_ROUNDS/NUM_POINTS*i
     )
-    point(point1[0], point1[1], point2[0], point2[1])
+    point2[0] += windowWidth/2
+    point2[1] += windowHeight/2
+    line(point1[0], point1[1], point2[0], point2[1])
     point1 = [...point2]
   }
   noLoop()
