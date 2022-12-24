1530da1f27afd578de4bab2a6ccd2ef35a23099e
pitossomo <pedro.h.teles@hotmail.com>
2022-12-23 23:54:07 -0300

    install react-p5

--- pages/arts/[art].tsx
+++ pages/arts/[art].tsx
@@ -1,6 +1,13 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
 import HomeLink from "../../components/HomeLink"
+import dynamic from 'next/dynamic'
+import p5Types from "p5"; //Import this for typechecking and intellisense
+
+const Sketch = dynamic(
+  () => import('react-p5').then((mod) => mod.default),
+  { ssr: false }
+)
 
 const Arts = () => {
   const router = useRouter()
@@ -13,7 +20,19 @@ const Arts = () => {
       ? word[0].toUpperCase() + word.slice(1)
       : word.toUpperCase()
     ).join(' ')
-  ) 
+  )
+
+  let [x, y] = [50, 50]; 
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+		p5.createCanvas(500, 500).parent(canvasParentRef);
+	};
+
+	const draw = (p5: p5Types) => {
+		p5.background(0);
+		p5.ellipse(x, y, 70, 70);
+		x++;
+	};
 
   return (
     <>
@@ -24,7 +43,7 @@ const Arts = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeLink />
-      <div className='canvasWrapper'>{ title }</div>
+      <Sketch setup={setup} draw={draw} />
     </>
   )
 }
